b10dd3b30e79bb342cb41df717235f631781ea50
Hakimi Ridzuan <mrhakimi88@gmail.com>
2020-02-16 21:10:17 +0800

    add Placepicker

---
+++ ShushmeApp/ShushmeApp/app/src/main/java/com/example/android/shushme/PlaceListAdapter.java
@@ -0,0 +1,49 @@
+package com.example.android.shushme;
+
+import android.content.Context;
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class PlaceListAdapter extends RecyclerView.Adapter<PlaceListAdapter.PlaceViewHolder> {
+
+    private Context mContext;
+
+    public PlaceListAdapter(Context context) {
+        this.mContext = context;
+    }
+
+    @Override
+    public PlaceViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // Get the RecyclerView item layout
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        View view = inflater.inflate(R.layout.item_place_card, parent, false);
+        return new PlaceViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(PlaceViewHolder holder, int position) {
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class PlaceViewHolder extends RecyclerView.ViewHolder {
+
+        TextView nameTextView;
+        TextView addressTextView;
+
+        public PlaceViewHolder(View itemView) {
+            super(itemView);
+            nameTextView = (TextView) itemView.findViewById(R.id.name_text_view);
+            addressTextView = (TextView) itemView.findViewById(R.id.address_text_view);
+        }
+
+    }
+}
